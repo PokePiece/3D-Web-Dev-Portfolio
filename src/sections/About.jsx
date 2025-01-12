b6d62d30999589f5b6432b0590c31c6e93b286f3
Dillon <DillonCarey333@@gmail.com>
2025-01-11 23:04:59 -0800

    first commit

---
+++ src/sections/About.jsx
@@ -0,0 +1,107 @@
+import React, { useState } from 'react'
+import Globe from 'react-globe.gl'
+import Button from '../components/Button'
+
+const About = () => {
+
+    const [hasCopied, setHasCopied] = useState(false)
+    
+    const handleCopy = () => {
+        navigator.clipboard.writeText('pokepiece33@gmail.com');
+
+        setHasCopied(true);
+        setTimeout(() => {
+            setHasCopied(false);
+        }, 2000);
+    }
+
+    return (
+        <section className="c-space my-20">
+            <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
+                <div className="col-span-1 xl:row-span-3">
+                    <div className="grid-container">
+                        <img src="/assets/chopin.jpg" alt="grid1" className="w-full sm:h-[276px]
+                    h-fit object-contain" />
+
+                        <div>
+                            <p className="grid-headtext">Hi, I'm PokePiece</p>
+                            <p className="grid-subtext">With 1 year of experience, I have honed my skills in
+                                frontend and backend development, with a
+                                focus on animated 3D websites.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-span-1 xl:row-span-3">
+                    <div className="grid-container">
+                        <img src="/assets/designpic.png" alt="grid2" className="w-full 
+                    sm:h-[276px] h-fit object-contain" />
+
+                        <div>
+                            <p className="grid-headtext">Tech Stack</p>
+                            <p className="grid-subtext">I use a wide range of technologies to build
+                                websites and applications. Some of the tools I use include React,
+                                Next.js, Tailwind CSS, and Framer Motion.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-span-1 xl:row-span-4">
+                    <div className='grid-container'>
+                        <div className='rounded-3xl w-full sm:[326px] h-fit flex justify-center items-center'>
+                            <Globe
+                                height={326}
+                                width={326}
+                                backgroundColor="rgba(0,0,0,0)"
+                                backgroundImageOpacity={0.5}
+                                showAtmosphere
+                                showGraticules
+                                globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+                                bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+                                labelsData={[{lat: 40, lng: -123, text: "I'm here",
+                                    color: 'white',
+                                    size: 20,
+                                }]}
+                            />
+                        </div>
+
+                        <div>
+                            <p className='grid-headtext'>
+                                I work remotely across most timezones.
+                            </p>
+                            <p className='grid-subtext'>
+                                I'm based in San Francisco, with remote work available.
+                            </p>
+                            <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
+                        </div>
+
+                    </div>
+                </div>
+                <div className="xl:col-span-2 xl:row-span-3">
+                    <div className="grid-container">
+                        <img src="/assets/grid3.png" alt="grid3" className="w-full sm:h-[266px] h-fit object-contain" />
+                        <div>
+                            <p className="grid-headtext">My Passion for Coding</p>
+                            <p className="grid-subtext">I'm a little passionate about coding and love to learn new things.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="xl:col-span-1 xl:row-span-2">
+                    <div className="grid-container">
+                        <img src="/assets/grid4.png" alt="grid4" className="w-full md:h-[126px] sm:h-[766px] h-fit object-cover sm:object-top" />
+                        
+                        <div className="space-y-2">
+                            <p className="grid-subtext text-center">Contact me</p>
+                            <div className="copy-container" onClick={handleCopy}>
+                                <img src={hasCopied ? 'assets/tick.svg' : 'assets/copy.svg'} alt="copy"/>
+                                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">pokepiece33@gmail.com</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default About
