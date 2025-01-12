6b265c30846500c33561d2a0f5fb6b0a9a4ec142
Dillon <DillonCarey333@@gmail.com>
2025-01-11 23:21:55 -0800

    second commit

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { myProjects } from './constants'
 
 const Projects = () => {
+
+    const currentProject = myProjects[0];
+
   return (
     <section className="c-space my-20">
         <p className="head-text">My Work</p>
@@ -13,7 +16,30 @@ const Projects = () => {
                 </div>
 
                 <div className="p-3 backdrop-filter backdrop-blur-3xl w-fit rounded-lg" style={myProjects[0].logoStyle}>
-                    <img src={myProjects[0].logo} alt="logo" className="w-10 h-10 shadow-sm" />
+                    <img src={currentProject.logo} alt="logo" className="w-10 h-10 shadow-sm" />
+                </div>
+                <div className='flex flex-col gap-5 text-white-600 my-5'>
+                    <p className="text-white text-2xl font-semibold animatedText">{currentProject.title}</p>
+                    <p className="animatedText">{currentProject.desc}</p>
+                    <p className="animatedText">{currentProject.subdesc}</p>
+                </div>
+                <div className="flex items-center justify-between flex-wrap gap-5">
+                    <div className='flex items-center gap-3'>
+                        {currentProject.tags.map((tag, index) => (
+                            <div key={index} className='tech-logo'>
+                                <img src={tag.path} alt={tag.name} />
+                            </div>
+                        ))}
+
+                    </div>
+                    <a className="flex items-center gap-2 cursor-pointer text-white-600" href={currentProject.href} target="_blank" rel="noreferrer">
+                        <p>Check Live Site</p>
+                        <img src="/assets/arrow-up.png" className="w-3 h-3" alt="arrow" />
+                    </a>
+                </div>
+
+                <div className="flex justify-between">
+
                 </div>
             </div>
         </div>
