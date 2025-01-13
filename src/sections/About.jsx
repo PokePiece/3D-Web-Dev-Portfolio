1672c67b1ba37b55e90a59c43e19205e73d67e3a
Dillon <DillonCarey333@@gmail.com>
2025-01-13 00:29:39 -0800

    formatted phone number

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -104,7 +104,7 @@ const About = () => {
                             </div>
                             <div className="copy-container" onClick={handleCopy}>
                                 <img src={hasCopied ? 'assets/tick.svg' : 'assets/copy.svg'} alt="copy" />
-                                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">925-278-0833</p>
+                                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">(925) 278-0833</p>
                             </div>
                         </div>
                     </div>
